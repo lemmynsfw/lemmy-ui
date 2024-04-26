fb4e4980de5cd47ff763213f377ed31c669a97b9
xaegreal <xaegreal@users.noreply.github.com>
2024-04-26 01:08:49 +0000

    Auto apply NSFW patch to 0.19.4-beta.5

--- src/server/utils/create-ssr-html.tsx
+++ src/server/utils/create-ssr-html.tsx
@@ -66,6 +66,20 @@ export async function createSsrHtml(
 
   const helmet = Helmet.renderStatic();
 
+  const adultConsent = site?.my_user
+    ? "<!-- User is logged in, dont ask -->"
+    : `
+  <script nonce="${cspNonce}">
+  if (localStorage.getItem('adult-consent') !== 'true') {
+    if (confirm(\`This website contains age-restricted materials including nudity and explicit depictions of sexual activity.\n\nBy entering, you affirm that you are at least 18 years of age or the age of majority in the jurisdiction you are accessing the website from and you consent to viewing sexually explicit content.\`)) {
+      localStorage.setItem('adult-consent', 'true')
+    } else {
+      alert("Alright, sending you back 👍")
+      history.back()
+    }
+  }
+  </script>
+`;
   const lazyScripts = [
     ...findTranslationChunkNames(userLanguages),
     ...findDateFnsChunkNames(userLanguages),
@@ -89,6 +103,9 @@ export async function createSsrHtml(
     </script>
     ${lazyScripts}
   
+    <!-- Ask for adult consent -->
+    ${adultConsent}
+
     <!-- A remote debugging utility for mobile -->
     ${erudaStr}
   
